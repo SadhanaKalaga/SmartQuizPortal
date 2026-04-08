7ada43d77bd7911f4fa7b9ae25d0469338f8023f
Sadhana <sadhanakalaga@gmail.com>
2026-04-08 10:43:51 +0530

    feat: create Angular quiz service for quiz management operations

---
+++ frontend/src/app/services/quiz.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+  private apiUrl = 'http://localhost:5000/api/quizzes';
+
+  constructor(private http: HttpClient) {}
+
+  createQuiz(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  getAllQuizzes(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  getQuizById(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  getMyQuizzes(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/my-quizzes`);
+  }
+
+  updateQuiz(id: string, data: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
+  }
+
+  deleteQuiz(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
